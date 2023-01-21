8f32fd4f005196b4ef6a938229b8463c350f6baa
Deivison Andre <117258034+deivisonandre@users.noreply.github.com>
2023-01-21 17:48:48 -0300

    Atualização curso aula05 aula06

---
+++ aula-curso/aula05/aula06a.js
@@ -0,0 +1,26 @@
+let caixaElement = document.querySelector('.caixa');
+
+caixaElement.style.width = '200px';
+caixaElement.style.height = '200px';
+caixaElement.style.backgroundColor = 'green';
+
+/*
+AS configurações de estilo via JavaScript seguem
+on mesmos conseitos e atributos ultilizados via CSS.
+
+OBS: Diferença
+
+Em CSS onde usa-se background-color
+Em JavaScript usa-se backgroundColor
+
+Outro exemplo:
+
+Em CSS onde usa-se font-size
+Em JavaScript usa-se fontSize
+
+Mais um exeplo:
+
+Em CSS onde usa-se margin-bottom
+Em JacaScript usa-se marginBottom
+
+*/
