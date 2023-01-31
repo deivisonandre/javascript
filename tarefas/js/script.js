a3ba34c4b046adcfd5e0d7d71e69837d5d6fae21
Deivison Andre <117258034+deivisonandre@users.noreply.github.com>
2023-01-31 18:53:59 -0300

    Atualização de tarefas

--- tarefas/js/script.js
+++ tarefas/js/script.js
@@ -7,6 +7,9 @@ let btn = document.querySelector('#botao');
 // 3) Temos que referenciar a lista 
 let lista = document.querySelector('#lista');
 
+// card
+let card = document.querySelector('.card');
+
 
 let tarefas = [
     'Jogar GTA5',
@@ -16,10 +19,69 @@ let tarefas = [
     'Ler um livro'
 ];
 
-/*
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Estudar Python</li>
-<li class="list-group-item list-group-item-action">Estudar JavaScript</li>
-<li class="list-group-item list-group-item-action">Ver um filme</li>
-<li class="list-group-item list-group-item-action">Ler um livro</li>
-*/
+function renderizarTarefas(){
+
+    // Limpar a listagem de itens antes de renderizar novamente a tela
+    lista.innerHTML = '';
+
+    for(tarefa of tarefas){
+        //Criar o item da lista
+        let itemLista = document.createElement('li');
+
+        //Adiciona classes no item da lista
+        itemLista.setAttribute('Class','list-group-item list-group-item-action');
+
+        // Criar um texto
+        let itemTexto = document.createTextNode(tarefa);
+
+        // Adicionar o texto no item da lista
+        itemLista.appendChild(itemTexto);
+
+        // Adicionar o item da lista na lista
+        lista.appendChild(itemLista);
+    }
+}
+
+// Executando a função para renderizar as tarefas
+renderizarTarefas();
+
+// 1) Precisamos "escutar" o evento de clique no botão
+btn.onclick = function(){
+    // 2) Precisamos capturar o valor digitado pelo usuário no input 
+    let novaTarefa = input.value;
+
+    if(novaTarefa !== ""){
+        // 3) Precisamos atualizar a nova tarefa na lista (array) de tarefas e renderizar a tela
+        tarefas.push(novaTarefa);
+
+        // Executando a função para renderizar as tarefas 
+        renderizarTarefas();
+
+        // Limpar o input
+        input.value = '';
+
+        // Limpar mensagens de erro (spans)
+        removerSpans();
+    }else{
+        // Limpar mensagens de erro (spans)
+        removerSpans();
+
+        let span = document.createElement('span');
+        span.setAttribute('class', 'Alert alert-warning');
+
+        let msg = document.createTextNode('Você precisa informa a tarefa!');
+
+        span.appendChild(msg);
+
+        card.appendChild(span);
+    }
+}
+
+
+function removerSpans(){
+    let spans = document.querySelectorAll('span');
+
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    }
+}
